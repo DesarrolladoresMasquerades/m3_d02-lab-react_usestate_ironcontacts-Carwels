946116aeb4d313baee95d7dee06df20ccfcb4f12
Carwi Barrios <carwelsbarrios@gmail.com>
2022-03-01 20:27:42 +0100

    lab done

--- src/App.js
+++ src/App.js
@@ -23,11 +23,32 @@ function App() {
     }
   }
 
+  const sortName = () => {
+    const arrFamous = [...famous]
+    const sortedFamous = arrFamous.sort((a, b) => {
+      return a.name.localeCompare(b.name)
+    })
+    setFamous(sortedFamous)
+  }
+
+  const sortPopularity = () => {
+    const arrFamous = [...famous]
+    const sortedPopularity = arrFamous.sort((a, b) => 
+    b.popularity - a.popularity
+    )
+    setFamous(sortedPopularity)
+  }
+
+  const deleteFamous = (id) => {
+    const deletedFamous = famous.filter(a => a.id !== id)
+    setFamous(deletedFamous)
+  }
+
   return (
     <div className="App">
     <button onClick={randomFamous}>Add Random Contact</button>
-    <button >Sort by popularity</button>
-    <button >Sort by name</button>
+    <button onClick={sortPopularity}>Sort by popularity</button>
+    <button onClick={sortName}>Sort by name</button>
      <table>
             <thead>
             <tr>
@@ -45,10 +66,11 @@ function App() {
             <tr key={contact.id}>
               <td><img src={contact.pictureUrl} alt="pictures" width="100px" height="100px"/></td>
              <td><h3>{contact.name}</h3></td>
-             <td>{contact.popularity}</td>
+             <td>{contact.popularity.toFixed(2)}</td>
              {contact.wonOscar &&<td>&#127942;</td>}
              {contact.wonEmmy &&<td>&#127942;</td>}
 
+             <td><button className="delete-btn" onClick={() => deleteFamous(contact.id)}>Delete Famous</button></td>
             </tr>
         )
       })}
